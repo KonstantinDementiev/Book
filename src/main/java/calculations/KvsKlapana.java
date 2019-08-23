cb4f9c2f7d6b4c2e4304fe85ea692e89161811c6
Konstantin Dementyev <dementyev@ukr.net>
2019-08-23 16:54:57 +0300

    calculations

---
+++ src/main/java/calculations/KvsKlapana.java
@@ -0,0 +1,16 @@
+package calculations;
+
+public class KvsKlapana {
+    private double flow;
+    private Integer pressureDrop;
+    private Integer temperature;
+    private Integer glikol;
+    private double densityMix;
+    private double kv;
+
+public double calcKv(){
+
+    return kv;
+}
+
+}
